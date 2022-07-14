3c231c0a2cf415b712777c07793899a862676a8b
Gawbill <959157@cognizant.com>
2022-07-13 22:18:34 -0500

    Final Version:

--- app/src/main/java/com/example/notifyme/MainActivity.kt
+++ app/src/main/java/com/example/notifyme/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.notifyme
 
-import android.R
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
-import android.provider.SyncStateContract.Helpers.update
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +19,7 @@ import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
     private val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+    private val ACTION_UPDATE_NOTIFICATION = "com.example.android.notifyme.ACTION_UPDATE_NOTIFICATION"
     private val NOTIFICATION_ID = 0
 
     lateinit var notifyButton: Button
@@ -24,42 +27,52 @@ class MainActivity : AppCompatActivity() {
     lateinit var update:Button
     lateinit var cancel:Button
 
+    var notificationReciever:NotificationReciever = NotificationReciever()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        notifyButton.findViewById<Button>(R.id.notify)
+        createNotificationChannel()
+        registerReceiver(notificationReciever, IntentFilter(ACTION_UPDATE_NOTIFICATION))
+
+        notifyButton = findViewById<Button>(R.id.notifyButton)
         notifyButton.setOnClickListener(View.OnClickListener {
             sendNotifcation()
         })
-        update.findViewById<Button>(R.id.update)
+        update = findViewById<Button>(R.id.update)
         update.setOnClickListener(View.OnClickListener {
             updateNotication()
         })
-        cancel.findViewById<Button>(R.id.cancel)
+        cancel = findViewById<Button>(R.id.cancel)
         cancel.setOnClickListener(View.OnClickListener {
             cancelNotification()
         })
 
-        //createNotificationChannel()
     }
-    fun sendNotifcation(){
+    fun createNotificationChannel(){
         notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
-            var notificationChannel:NotificationChannel = NotificationChannel(PRIMARY_CHANNEL_ID, "Mascot Notification", NotificationManager.IMPORTANCE_HIGH)
-            notificationChannel.lightColor = Color.RED
-            notificationChannel.enableLights(true)
+            var notificationChannel:NotificationChannel = NotificationChannel(PRIMARY_CHANNEL_ID, "Mascot Notificaiton", NotificationManager.IMPORTANCE_HIGH)
             notificationChannel.enableVibration(true)
-            notificationChannel.description = "Notification from Mastcot"
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.description = "Notification from mascot"
             notificationManager.createNotificationChannel(notificationChannel)
-
-            val notifyBuilder = getNotificationBuilder()
-            notificationManager.notify(NOTIFICATION_ID, notifyBuilder!!.build())
-
-            setNotificationButtonState(true, false, false);
         }
     }
-    private fun getNotificationBuilder(): NotificationCompat.Builder? {
+    fun sendNotifcation(){
+        var intent:Intent = Intent(ACTION_UPDATE_NOTIFICATION)
+        var pendingIntent:PendingIntent = PendingIntent.getBroadcast(this, NOTIFICATION_ID, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        var notifyBuilder:NotificationCompat.Builder = getNotificationBuilder()
+        notifyBuilder.addAction(R.drawable.ic_update, "Update Notification", pendingIntent)
+        notificationManager.notify(NOTIFICATION_ID, notifyBuilder.build())
+
+
+
+    }
+    private fun getNotificationBuilder(): NotificationCompat.Builder {
         //Intent to launch the main activity
         val notificationIntent = Intent(this, MainActivity::class.java)
         //Creates a pending intent (an intent that allows it be fired on behalf of you)
@@ -81,9 +94,9 @@ class MainActivity : AppCompatActivity() {
         return notifyBuilder
     }
     fun updateNotication(){
-        val androidImage = BitmapFactory.decodeResource(resources, R.drawable.mascot_1)
-        val notifyBuilder = getNotificationBuilder()
-        notifyBuilder!!.setStyle(
+        val androidImage:Bitmap = BitmapFactory.decodeResource(resources, R.drawable.mascot_1)
+        val notifyBuilder:NotificationCompat.Builder = getNotificationBuilder()
+        notifyBuilder.setStyle(
             NotificationCompat.BigPictureStyle()
                 .bigPicture(androidImage)
                 .setBigContentTitle("Notification Updated!")
@@ -109,5 +122,15 @@ class MainActivity : AppCompatActivity() {
         if (isCancelEnabled != null) {
             notifyButton.setEnabled(isCancelEnabled)
         }
+    }
+    override fun onDestroy() {
+        unregisterReceiver(notificationReciever)
+        super.onDestroy()
+    }
+    inner class NotificationReciever:BroadcastReceiver(){
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            updateNotication()
+        }
+
     }
 }
